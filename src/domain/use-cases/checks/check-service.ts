d55502800fcd1255ba36547806d09852df82b743
David <davidgranados73@gmail.com>
2023-11-08 20:16:56 -0500

    CheckService - UseCase

---
+++ src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,20 @@
+interface CheckServiceUseCae {
+  execute(url: string): Promise<boolean>;
+}
+
+export class CheckService implements CheckServiceUseCae {
+  async execute(url: string): Promise<boolean> {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Error on check service ${url}`);
+      }
+      console.log(`Check service ${url} is ok`);
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,11 +1,12 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
 export class Server {
   public static start(): void {
     console.log("Server started");
 
-    CronService.createJob("*/2 * * * * *", () => {
-      console.log("Cron job executed" + new Date());
+    CronService.createJob("*/5 * * * * *", () => {
+      new CheckService().execute("https://www.google.com");
     });
   }
 }
