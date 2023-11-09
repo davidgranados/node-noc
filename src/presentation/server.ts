81806c6b0a19807909f0c6818dd998e2c36024b3
David <davidgranados73@gmail.com>
2023-11-08 20:07:41 -0500

    CronService

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,12 +1,11 @@
-import { CronJob } from "cron";
+import { CronService } from "./cron/cron-service";
 
 export class Server {
   public static start(): void {
     console.log("Server started");
 
-    const job = new CronJob("* * * * * *", () => {
-      console.log("You will see this message every second");
+    CronService.createJob("*/2 * * * * *", () => {
+      console.log("Cron job executed" + new Date());
     });
-    job.start();
   }
 }
