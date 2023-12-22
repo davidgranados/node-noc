c84ed554f41ea23ff0a4f2af2148a510be1165ea
David <davidgranados73@gmail.com>
2023-12-22 13:50:30 -0500

    Grabar Logs en Mongo

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,13 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
-// import { SendLogsEmail } from "../domain/use-cases/send-logs";
 import { FileSystemDataSource } from "../infrastructure/data-sources/file-system.data-source";
+import { MongoLogDataSource } from "../infrastructure/data-sources/mongo-log.data-source";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
-// import { EmailService } from "./email/email.service";
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDataSource()
+const logRepository = new LogRepositoryImpl(
+  // new FileSystemDataSource()
+  new MongoLogDataSource()
 );
-// const emailService = new EmailService();
 
 export class Server {
   public static start(): void {
@@ -16,11 +15,7 @@ export class Server {
 
     CronService.createJob("*/5 * * * * *", () => {
       const url = "http://localhost:3000/";
-      new CheckService(fileSystemLogRepository).execute(url);
+      new CheckService(logRepository).execute(url);
     });
-
-    // new SendLogsEmail(fileSystemLogRepository, emailService).execute(
-    //   ["davidgranados73@gmail.com"]
-    // );
   }
 }
