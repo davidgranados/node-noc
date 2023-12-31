dc39eb8c4bd8f103bc508ea55d4f677ece8d779a
David <davidgranados73@gmail.com>
2023-12-31 11:19:54 -0500

    insertar y leer postgres

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 import { envs } from "./config/plugins/envs.plugins";
 import { MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
+import { getCurrentFilename } from "./utils";
+
+const filename = getCurrentFilename(__filename);
 
 async function main() {
   await MongoDatabase.connect({
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME,
   });
+
+  const prisma = new PrismaClient();
+  // const newLog = await prisma.logModel.create({
+  //   data: {
+  //     message: "Hello",
+  //     level: "CRITICAL",
+  //     origin: filename,
+  //   },
+  // });
+
+  // console.log(newLog);
+
+  const logs = await prisma.logModel.findMany({
+    where: {
+      level: "CRITICAL",
+    },
+  });
+  console.log(logs);
 
   Server.start();
 }
